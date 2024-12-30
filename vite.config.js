1ddf368bfae0620d38fbe55a9c8cc3a3952dbf5b
Mike Gurba <mihailgurba@gmail.com>
2024-12-30 21:21:21 +0100

    fix(token-security): token updated, styles updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/portfolio/',
+  define: {
+    'process.env': process.env,
+  },
   css: {
     preprocessorOptions: {
       scss: {
